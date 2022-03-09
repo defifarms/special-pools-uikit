d40d37716e530f87477fd10286d8b7c2d79e3d4f
Nguyễn Văn Dũng <>
2022-03-09 23:22:20 +0700

    Update ui kit

--- src/components/Image/TokenImage.tsx
+++ src/components/Image/TokenImage.tsx
@@ -4,7 +4,6 @@ import Image from "./Image";
 const TokenImage = styled(Image)`
   &:before {
     border-radius: 50%;
-    border: 1px solid rgba(0, 0, 0, 0.25);
     content: "";
     height: 100%;
     left: 0;
